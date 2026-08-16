d391c027ee2e3ac0e79bad07c48daf10e0dee68b
Maruf995git <mamadalievmaruf740@gmail.com>
2026-08-17 02:10:48 +0600

    add main page

--- view_pages/urls.py
+++ view_pages/urls.py
@@ -1,10 +1,21 @@
 
 from django.urls import path
 
-from view_pages.views import main_page
+from view_pages.views import history_page, law_page, main_page, department_page, readership_page, \
+    services_page, vacancies_page, volunteer_page, contact_page, events_page, events_detail_page
 
 
 
 urlpatterns = [
     path('', main_page, name='main-page'),
+    path('history/', history_page, name='history-page'),
+    path('departments/', department_page, name='department-page'),
+    path('services/', services_page, name='services-page'),
+    path('vacancies/', vacancies_page, name='vacancies-page'),
+    path('law/', law_page, name='law-page'),
+    path('volunteer/', volunteer_page, name='volunteer-page'),
+    path('readership/', readership_page, name='readership-page'),
+    path('contact/', contact_page, name='contact-page'), 
+    path('events/', events_page, name='events_page'),
+    path('events/<int:event_id>/', events_detail_page, name='events-detail-page'),
 ]
--- view_pages/views.py
+++ view_pages/views.py
@@ -1,6 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import TemplateView
+from .models import Event
 
 # Create your views here.
 def main_page(request):
     return render(request, 'main-page.html')
+
+def history_page(request):
+    return render(request, 'history.html')
+
+def department_page(request):
+    return render(request, 'departments.html')
+
+def services_page(request):
+    return render(request, 'services.html')
+
+def vacancies_page(request):
+    return render(request, 'vacancies.html')
+
+def law_page(request):
+    return render(request, 'law.html')
+
+def volunteer_page(request):
+    return render(request, 'volunteer.html')
+
+def readership_page(request):
+    return render(request, 'readership.html')
+
+def contact_page(request):
+    return render(request, 'contact.html')
+
+
+def events_page(request):
+# Получаем уникальные даты, в которые есть мероприятия (возвращает список datetime.date)
+    event_dates = Event.objects.dates('date', 'day').order_by('date')
+    
+    # Базовый QuerySet со всеми мероприятиями
+    events = Event.objects.all().order_by('date')
+    
+    # Проверяем, есть ли фильтрация по дате в URL
+    selected_date = request.GET.get('date')
+    if selected_date:
+        # Фильтруем мероприятия по конкретному дню
+        events = events.filter(date__date=selected_date)
+
+    context = {
+        'events': events,
+        'event_dates': event_dates,
+        'selected_date': selected_date,
+    }
+    return render(request, 'events.html', context)
+
+def events_detail_page(request, event_id):
+    # Достаем мероприятие по ID или выдаем 404 страницу, если его нет
+    event = get_object_or_404(Event, id=event_id)
+    
+    context = {
+        'event': event,
+    }
+    return render(request, 'events_detail.html', context)
